309ff24e5231743052032a57ab0ffb071c755f6b
Zorg-Moris <mvr.leon@gmail.com>
2020-04-18 12:20:02 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-module.exports.equalToZero = function(array) {
-    let maxNum = Math.max(...array);
-    let sumAcc = array.reduce(function (sum, current) {
-      return sum + current;
-    });
-  
-    if (maxNum > sumAcc / 2 || sumAcc % 2 !== 0) {
-      return "NO";
-    } else {
-      return "YES";
-    }
-  }
+module.exports.equalToZero = function (array) {
+  let maxNum = Math.max(...array);
+  let sumAcc = array.reduce(function (sum, current) {
+    return sum + current;
+  });
+
+  if(sumAcc % 2 === 0 && 2 * maxNum <= sumAcc) {
+    return "YES";
+  } 
+   return "NO"; 
+}
